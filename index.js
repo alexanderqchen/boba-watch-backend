c75aaff761f3e68502c6fb72990a287461c28fd2
alexanderqchen <aqchen@ucla.edu>
2019-03-30 13:41:28 -0700

    include body parser

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const usersRouter = require('./routes/users');
 const drinksRouter = require('./routes/drinks');
 
 const PORT = 80;
 
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/users', usersRouter);
 app.use('/drinks', drinksRouter);
 
