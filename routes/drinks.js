bd010d854296eb5431ee4e64994e7836afa75419
alexanderqchen <aqchen@ucla.edu>
2019-03-30 13:16:44 -0700

    commented code

--- routes/drinks.js
+++ routes/drinks.js
@@ -6,13 +6,13 @@ const Drinks = require('../models/drinks');
 router.route('/')
 // Get drinks from user
 .get(async (req, res, next) => {
-	try {
-		const drinks = await getDrinks();
-		res.status(200).json(drinks);
-	}
-	catch {
-		res.status(500).json();
-	}
+	// try {
+	// 	const drinks = await getDrinks();
+	// 	res.status(200).json(drinks);
+	// }
+	// catch {
+	// 	res.status(500).json();
+	// }
 })
 // Add drink to user
 .post((req, res, next) => {
@@ -34,27 +34,27 @@ router.route('/')
 })
 // Update drink
 .put((req, res, next) => {
-	let drink = req.body.drink;
+	// let drink = req.body.drink;
 
-	try {
-		drink = await updateDrink(drink);
-		res.status(200).json(drink);
-	}
-	catch {
-		res.status(500).json();
-	}
+	// try {
+	// 	drink = await updateDrink(drink);
+	// 	res.status(200).json(drink);
+	// }
+	// catch {
+	// 	res.status(500).json();
+	// }
 })
 // Delete drink from user
 .delete((req, res, next) => {
-	const drinkId = req.params.drinkId;
+	// const drinkId = req.params.drinkId;
 
-	try {
-		const drink = await deleteDrink(drinkId);
-		res.status(200).json(drink);
-	}
-	catch {
-		res.status(500).json();
-	}
+	// try {
+	// 	const drink = await deleteDrink(drinkId);
+	// 	res.status(200).json(drink);
+	// }
+	// catch {
+	// 	res.status(500).json();
+	// }
 });
 
 module.exports = router;
