88b6da361745a999ce560a49e8af6a25776e1c94
alexanderqchen <aqchen@ucla.edu>
2019-03-30 15:43:31 -0700

    updated model for user

--- models/users.js
+++ models/users.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define('Users', {
-    facebookUserId: DataTypes.STRING
+    facebookUserId: DataTypes.STRING,
+    budget: DataTypes.INTEGER,
+    maxDrinks: DataTypes.INTEGER,
   }, {});
   Users.associate = function(models) {
     // associations can be defined here
