2d04ab44c28fbe4d28f6a7c360d75dd64ca4c4a8
alexanderqchen <aqchen@ucla.edu>
2019-03-30 13:19:40 -0700

    bugfix?

--- routes/drinks.js
+++ routes/drinks.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Drinks = require('../models/drinks');
+const Drinks = require('../models').Drinks;
 
 router.route('/')
 // Get drinks from user
