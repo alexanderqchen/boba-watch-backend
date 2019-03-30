ff4a340d02c4e22620487503f19320343832224e
alexanderqchen <aqchen@ucla.edu>
2019-03-30 13:15:07 -0700

    update drinks controller

--- routes/drinks.js
+++ routes/drinks.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Drinks = require('../models/drinks');
+
 router.route('/')
 // Get drinks from user
 .get(async (req, res, next) => {
@@ -14,15 +16,21 @@ router.route('/')
 })
 // Add drink to user
 .post((req, res, next) => {
-	let drink = req.body.drink;
+	// let drink = req.body.drink;
 
-	try {
-		drink = await addDrink(drink);
-		res.status(200).json(drink);
-	}
-	catch {
-		res.status(500).json();
-	}
+	Drinks.create({
+		name: 'test1',
+		location: 'testloc',
+		price: 123,
+		date: new Date(),
+		photo: 'asdf'
+	})
+	.then(drink => {
+		res.status(201).json(drink);
+	})
+	.catch(err => {
+		res.status(400).json(err);
+	});
 })
 // Update drink
 .put((req, res, next) => {
