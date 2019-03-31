6cbf20c6dae62ffdb51b4fae6bac63a0c03757c8
Alexander Chen <aqchen@ucla.edu>
2019-03-30 18:49:36 -0700

    Created Dashboard API (#5)
    * order drinks by date
    
    * created dashboard api
    
    * fixed bug
    
    * implement monthly dashboard data
    
    * fixed bug
    
    * i think i gixed compare bug
    
    * test
    
    * hacky fix
    
    * does this work
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * asdf
    
    * i think its fixed

---
+++ routes/dashboard.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+const Drinks = require('../models').Drinks;
+const Users = require('../models').Users;
+
+router.route('/:userId')
+// Get all drinks
+.get(async (req, res, next) => {
+	const userId = req.params.userId;
+	const now = new Date();
+	const beginningOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+
+	try {
+		const drinks = await Drinks.findAll({
+			where: { userId }
+		});
+
+		let spent = 0;
+		let numDrinks = 0;
+
+		drinks.forEach((drink) => {
+			console.log(beginningOfMonth)
+			if (new Date(drink.date) > beginningOfMonth) {
+				spent += drink.price;
+				numDrinks++;
+			}
+		});
+		const user = await Users.findOne({ where: { id: userId } });
+
+		dashboard = {
+			budget: user.budget,
+			spent,
+			maxDrinks: user.maxDrinks,
+			numDrinks
+		}
+		res.status(200).json(dashboard)
+	}
+	catch (err) {
+		err.message = 'error with GET /dashboard/:userId';
+		res.status(400).json(err);
+	}
+});
+
+module.exports = router;
