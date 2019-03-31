d951e87ba2e1205ceb3037fccbffd32c724f27ed
Alexander Chen <aqchen@ucla.edu>
2019-03-30 22:06:14 -0700

    Added https support (#6)
    * added root endpoint
    
    * configured key and cert with express
    
    * finished https

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
 
 const usersRouter = require('./routes/users');
 const drinksRouter = require('./routes/drinks');
 const dashboardRouter = require('./routes/dashboard')
 
-const PORT = 80;
+const PORT = 443;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -19,6 +21,12 @@ app.use('/users', usersRouter);
 app.use('/drinks', drinksRouter);
 app.use('/dashboard', dashboardRouter);
 
-app.listen(PORT, (req, res, next) => {
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.boba.watch/privkey.pem');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/api.boba.watch/cert.pem');
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(PORT, (req, res, next) => {
 	console.log(`Listening on port ${PORT}...`);
 });
