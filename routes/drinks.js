3c7e384357defa342b1e5e67c0b9062d51e34bd2
alexanderqchen <aqchen@ucla.edu>
2019-03-31 09:17:31 -0700

    auth for post drink

--- routes/drinks.js
+++ routes/drinks.js
@@ -16,17 +16,45 @@ router.route('/')
 	.catch(err => {
 		res.status(400).json(err);
 	});
-})
-// Add drink
-.post((req, res, next) => {
-	let drink = req.body.drink;
+});
 
-	Drinks.create(drink)
-	.then(drink => {
-		res.status(200).json(drink);
-	})
-	.catch(err => {
-		res.status(400).json(err);
+// Add drink
+router.post('/:accessToken', (req, res, next) => {
+	const accessToken = req.params.accessToken;
+	const drink = req.body.drink;
+
+	const userId = drink.userId;
+
+	axios.get(`https://graph.facebook.com/debug_token?input_token=${accessToken}&access_token=${appId}|${appSecret}`)
+	.then(authRes => {
+		const is_valid = authRes.data.data.is_valid;
+		const facebookUserId = authRes.data.data.user_id;
+
+		if (is_valid) {
+			Users.findOne({ where: { facebookUserId } })
+			.then(user => {
+				if (userId == user.id) {
+					Drinks.create(drink)
+					.then(drink => {
+						res.status(200).json(drink);
+					})
+					.catch(err => {
+						res.status(400).json(err);
+					});
+				}
+				else {
+					res.status(401).json({
+						msg: `Unauthorized to user ${id}`
+					})
+				}
+			})
+		}
+		else {
+			console.log(4)
+			res.status(400).json({
+				msg: "Authentication failed."
+			})
+		}
 	});
 });
 
