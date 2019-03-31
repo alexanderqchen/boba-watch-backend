1a1d3c1bee98fe1d78bf3929e123bda8b056d2ab
Alexander Chen <aqchen@ucla.edu>
2019-03-30 17:31:14 -0700

    Enable CORS Access (#4)

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const drinksRouter = require('./routes/drinks');
 const PORT = 80;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use('/users', usersRouter);
 app.use('/drinks', drinksRouter);
 
