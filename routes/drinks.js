e21418f751082ac1d0ee59cb02a77aa3be16672e
alexanderqchen <aqchen@ucla.edu>
2019-03-31 10:45:48 -0700

    update delete to post

--- routes/drinks.js
+++ routes/drinks.js
@@ -127,8 +127,45 @@ router.route('/:id/:accessToken')
 		res.status(400).json(err);
 	});
 })
+
+router.route('delete/:id/:accessToken')
+.all((req, res, next) => {
+	const drinkId = req.params.id;
+	const accessToken = req.params.accessToken;
+	Drinks.findOne({ where: { id: drinkId } })
+	.then(drink => {
+		const userId = drink.userId;
+		axios.get(`https://graph.facebook.com/debug_token?input_token=${accessToken}&access_token=${appId}|${appSecret}`)
+		.then(authRes => {
+			const is_valid = authRes.data.data.is_valid;
+			const facebookUserId = authRes.data.data.user_id;
+
+			if (is_valid) {
+				Users.findOne({ where: { facebookUserId } })
+				.then(user => {
+					if (userId == user.id) {
+						return next();
+					}
+					else {
+						res.status(401).json({
+							msg: `Unauthorized to user ${id}`
+						})
+					}
+				})
+			}
+			else {
+				res.status(400).json({
+					msg: "Authentication failed."
+				})
+			}
+		})
+	})
+	.catch(err => {
+		res.status(400).json(err);
+	});
+})
 // Delete drink from user
-.delete((req, res, next) => {
+.post((req, res, next) => {
 	const drinkId = req.params.id;
 
 	Drinks.destroy({ where: { id: drinkId } })
