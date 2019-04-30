742197437f258455e27187ecfb5585f26404385c
Alexander Chen <alexander.qili.chen@gmail.com>
2019-04-30 16:46:39 -0700

    Created separate environments for prod and dev (#22)
    * separated dev and prod envs
    
    * fixed start commands
    
    * fixed killing pid

--- Makefile
+++ Makefile
@@ -1,6 +1,24 @@
+PROJECT_NAME=boba-watch
+ZONE=us-west2-b
+INSTANCE_NAME=instance-1
+
+SQL_SERVER_IP=35.236.79.15
+SQL_INSTANCE_CONNECTION_NAME=boba-watch:us-west2:instance0
+SQL_USER=root
+
+prod:
+	sudo kill -9 $(shell sudo netstat -nlp | grep 443 | sed s/.*LISTEN// | tr -dc 0-9)
+	npm start
+dev:
+	nodemon index.js
 ssh:
-	gcloud compute --project "boba-watch" ssh --zone "us-west2-b" "instance-1"
+	gcloud compute ssh --project $(PROJECT_NAME) --zone $(ZONE) $(INSTANCE_NAME)
 sql:
-	mysql --host=35.236.79.15 --user=root --password
-reset-database:
-	mysql --host=35.236.79.15 --user=root --password < ./scripts/db-setup.sql
+	mysql --host=$(SQL_SERVER_IP) --user=$(SQL_USER) --password
+sqlproxy:
+	# To setup cloud_sql_proxy follow instructions here: https://cloud.google.com/sql/docs/mysql/quickstart-proxy-test
+	# Then add ./cloud_sql_proxy to /user/local/bin
+	cloud_sql_proxy -instances=$(SQL_INSTANCE_CONNECTION_NAME)=tcp:3306
+sqldev:
+	# Run `make sqlproxy` in a separate terminal window before running this
+	mysql --user=$(SQL_USER) --password --host 127.0.0.1 --port 3306
--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const usersRouter = require('./routes/users');
 const drinksRouter = require('./routes/drinks');
 const dashboardRouter = require('./routes/dashboard')
 
-const PORT = 443;
+const NODE_ENV = process.env.NODE_ENV;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
 
@@ -22,19 +22,33 @@ app.use('/users', usersRouter);
 app.use('/drinks', drinksRouter);
 app.use('/dashboard', dashboardRouter);
 
-
 app.all('/*', (req, res, next) => {
 	res.json({
-		msg: "Read the API documentation here: https://github.com/alexanderqchen/boba-watch-backend"
+		msg: 'Read the API documentation here: https://github.com/alexanderqchen/boba-watch-backend'
 	});
 });
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.boba.watch/privkey.pem');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/api.boba.watch/cert.pem');
+if (NODE_ENV == 'production') {
+	const PORT = 443;
 
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(PORT, (req, res, next) => {
-	console.log(`Listening on port ${PORT}...`);
-});
+	const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.boba.watch/privkey.pem');
+	const certificate = fs.readFileSync('/etc/letsencrypt/live/api.boba.watch/cert.pem');
+
+	https.createServer({
+		key: privateKey,
+		cert: certificate
+	}, app).listen(PORT, (req, res, next) => {
+		console.log(`Listening on port ${PORT}...`);
+	});
+}
+else if (NODE_ENV == 'development') {
+	const PORT = 8000;
+
+	app.listen(PORT, (req, res, next) => {
+		console.log(`Listening on port ${PORT}...`);
+	});
+}
+else {
+	console.log('Error: The environment variable NODE_ENV must be set to either `prod` or `dev`.');
+	process.exit(1);
+}
