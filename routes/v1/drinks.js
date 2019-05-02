4e4327e308b06e95ed317ad66c62ea83a715cc6a
alexanderqchen <aqchen@ucla.edu>
2019-05-01 19:38:57 -0700

    converted to async await

--- routes/v1/drinks.js
+++ routes/v1/drinks.js
@@ -8,6 +8,10 @@ const setUserId = async (req, res, next) => {
 	const drinkId = req.params.drinkId;
 	const drink = await Drinks.findOne({ where: { id: drinkId } });
 
+	if (drink == null) {
+		res.status(400).json({ msg: 'Invalid drink id.' });
+	}
+
 	req.params.userId = drink.userId;
 	next();
 };
@@ -52,46 +56,31 @@ router.route('/user/:userId')
 	const drinks = await getDrinksForUser(userId);
 	res.status(200).json(drinks);
 })
-.post(sanitizeDrink, (req, res, next) => {
+.post(sanitizeDrink, async (req, res, next) => {
 	const userId = req.params.userId;
 	const drink = req.body.drink;
-
 	drink.userId = userId;
 
-	Drinks.create(drink)
-	.then(drink => {
-		res.status(200).json(drink);
-	})
-	.catch(err => {
-		res.status(400).json(err);
-	});
+	const newDrink = await Drinks.create(drink);
+	res.status(200).json(newDrink);
 });
 
 router.route('/:drinkId')
 .all(setUserId, authorizeUser)
 // Update drink
-.put(sanitizeDrink, (req, res, next) => {
+.put(sanitizeDrink, async (req, res, next) => {
 	const drinkId = req.params.drinkId;
 	const drink = req.body.drink;
 
-	Drinks.update(drink, { where: { id: drinkId } })
-	.then(counts => {
-		res.status(200).json(counts[0]);
-	})
-	.catch(err => {
-		res.status(400).json(err);
-	});
+	const counts = await Drinks.update(drink, { where: { id: drinkId } });
+	res.status(200).json(counts[0]);;
 })
-.delete((req, res, next) => {
+// Delete drink
+.delete(async (req, res, next) => {
 	const drinkId = req.params.drinkId;
 
-	Drinks.destroy({ where: { id: drinkId } })
-	.then(count => {
-		res.status(200).json(count);
-	})
-	.catch(err => {
-		res.status(400).json(err);
-	});
-})
+	const count = await Drinks.destroy({ where: { id: drinkId } });
+	res.status(200).json(count);
+});
 
 module.exports = router;
